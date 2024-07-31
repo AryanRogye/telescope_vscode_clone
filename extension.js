5fb11903a0b4585e75dc9dcae88e1df350d9daef
AryanRogye <aryan.rogye@gmail.com>
2024-07-31 13:23:03 -0500

    added basic file structure

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require("vscode");
 const { grepFiles } = require("./src/fuzzy_finder/views/find_files");
-const { GrepSearch } = require("./src/grep-files/views/grep-search");
+const { GrepSearch } = require("./src/grep-search/views/grep-search");
 
 function activate(context) {
   const disposable1 = vscode.commands.registerCommand(
